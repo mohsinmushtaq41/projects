f9876a8e6e342cad390a2a8f0cdcddc1a79a6708
mohsinmushtaq41 <mohsinmushtaq4141@gmail.com>
2019-02-19 21:52:50 +0500

    Added Calculater

---
+++ CalculaterPro/CalculaterPro/CalculaterProgram.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculater
+{
+    public class CalculaterProgram
+    {
+        public int Add(int num1 , int num2)
+        {
+            int result = num1 + num2;
+            return result;
+        }
+        public int Sub(int num1, int num2)
+        {
+            int result = num1 - num2;
+            return result;
+        }
+    }
+}
---
+++ CalculaterPro/CalculaterTest/CalculaterProgramTest.cs
@@ -0,0 +1,41 @@
+﻿using Calculater;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaterTest
+{
+    [TestClass]
+    class CalculaterProgramTest
+    {
+        [TestMethod]
+        public void AddTest()
+        {
+            // Arrange data
+            CalculaterProgram cal = new CalculaterProgram();
+
+            // Act
+            int result = cal.Add(10,5);
+            // Assert
+            Assert.AreEqual(15,result);
+          
+
+        }
+        [TestMethod]
+        public void SubTest()
+        {
+            // Arange
+            CalculaterProgram cal = new CalculaterProgram();
+            
+            // Act
+            int result = cal.Sub(30, 5);
+            // Assert
+            Assert.AreEqual(25,result);
+
+        }
+
+    }
+}
